f7f80db49d23483f2a8f6037fd4dc5654ce31c62
Paul Anyebe <p.anyebe9@gmail.com>
2024-07-26 13:14:21 +0100

    Manager

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,7 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.labelFirstName = new System.Windows.Forms.Label();
             this.labelLastName = new System.Windows.Forms.Label();
             this.labelPhoneNumber = new System.Windows.Forms.Label();
@@ -48,81 +49,90 @@
             // labelFirstName
             // 
             this.labelFirstName.AutoSize = true;
-            this.labelFirstName.Location = new System.Drawing.Point(12, 15);
+            this.labelFirstName.Location = new System.Drawing.Point(9, 12);
+            this.labelFirstName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelFirstName.Name = "labelFirstName";
-            this.labelFirstName.Size = new System.Drawing.Size(76, 17);
+            this.labelFirstName.Size = new System.Drawing.Size(57, 13);
             this.labelFirstName.TabIndex = 0;
             this.labelFirstName.Text = "First Name";
             // 
             // labelLastName
             // 
             this.labelLastName.AutoSize = true;
-            this.labelLastName.Location = new System.Drawing.Point(12, 43);
+            this.labelLastName.Location = new System.Drawing.Point(9, 35);
+            this.labelLastName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelLastName.Name = "labelLastName";
-            this.labelLastName.Size = new System.Drawing.Size(76, 17);
+            this.labelLastName.Size = new System.Drawing.Size(58, 13);
             this.labelLastName.TabIndex = 1;
             this.labelLastName.Text = "Last Name";
             // 
             // labelPhoneNumber
             // 
             this.labelPhoneNumber.AutoSize = true;
-            this.labelPhoneNumber.Location = new System.Drawing.Point(12, 71);
+            this.labelPhoneNumber.Location = new System.Drawing.Point(9, 58);
+            this.labelPhoneNumber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelPhoneNumber.Name = "labelPhoneNumber";
-            this.labelPhoneNumber.Size = new System.Drawing.Size(103, 17);
+            this.labelPhoneNumber.Size = new System.Drawing.Size(78, 13);
             this.labelPhoneNumber.TabIndex = 2;
             this.labelPhoneNumber.Text = "Phone Number";
             // 
             // labelEmail
             // 
             this.labelEmail.AutoSize = true;
-            this.labelEmail.Location = new System.Drawing.Point(12, 99);
+            this.labelEmail.Location = new System.Drawing.Point(9, 80);
+            this.labelEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelEmail.Name = "labelEmail";
-            this.labelEmail.Size = new System.Drawing.Size(42, 17);
+            this.labelEmail.Size = new System.Drawing.Size(32, 13);
             this.labelEmail.TabIndex = 3;
             this.labelEmail.Text = "Email";
             // 
             // textBoxFirstName
             // 
-            this.textBoxFirstName.Location = new System.Drawing.Point(121, 12);
+            this.textBoxFirstName.Location = new System.Drawing.Point(91, 10);
+            this.textBoxFirstName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxFirstName.Name = "textBoxFirstName";
-            this.textBoxFirstName.Size = new System.Drawing.Size(271, 22);
+            this.textBoxFirstName.Size = new System.Drawing.Size(204, 20);
             this.textBoxFirstName.TabIndex = 4;
             // 
             // textBoxLastName
             // 
-            this.textBoxLastName.Location = new System.Drawing.Point(121, 40);
+            this.textBoxLastName.Location = new System.Drawing.Point(91, 32);
+            this.textBoxLastName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxLastName.Name = "textBoxLastName";
-            this.textBoxLastName.Size = new System.Drawing.Size(271, 22);
+            this.textBoxLastName.Size = new System.Drawing.Size(204, 20);
             this.textBoxLastName.TabIndex = 5;
             // 
             // textBoxPhoneNumber
             // 
-            this.textBoxPhoneNumber.Location = new System.Drawing.Point(121, 68);
+            this.textBoxPhoneNumber.Location = new System.Drawing.Point(91, 55);
+            this.textBoxPhoneNumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxPhoneNumber.Name = "textBoxPhoneNumber";
-            this.textBoxPhoneNumber.Size = new System.Drawing.Size(271, 22);
+            this.textBoxPhoneNumber.Size = new System.Drawing.Size(204, 20);
             this.textBoxPhoneNumber.TabIndex = 6;
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Location = new System.Drawing.Point(121, 96);
+            this.textBoxEmail.Location = new System.Drawing.Point(91, 78);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(271, 22);
+            this.textBoxEmail.Size = new System.Drawing.Size(204, 20);
             this.textBoxEmail.TabIndex = 7;
             // 
             // listBoxContacts
             // 
             this.listBoxContacts.FormattingEnabled = true;
-            this.listBoxContacts.ItemHeight = 16;
-            this.listBoxContacts.Location = new System.Drawing.Point(15, 124);
+            this.listBoxContacts.Location = new System.Drawing.Point(11, 101);
+            this.listBoxContacts.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBoxContacts.Name = "listBoxContacts";
-            this.listBoxContacts.Size = new System.Drawing.Size(377, 196);
+            this.listBoxContacts.Size = new System.Drawing.Size(284, 160);
             this.listBoxContacts.TabIndex = 8;
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(398, 11);
+            this.buttonAdd.Location = new System.Drawing.Point(298, 9);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(94, 23);
+            this.buttonAdd.Size = new System.Drawing.Size(70, 19);
             this.buttonAdd.TabIndex = 9;
             this.buttonAdd.Text = "Add";
             this.buttonAdd.UseVisualStyleBackColor = true;
@@ -130,9 +140,10 @@
             // 
             // buttonEdit
             // 
-            this.buttonEdit.Location = new System.Drawing.Point(398, 40);
+            this.buttonEdit.Location = new System.Drawing.Point(298, 32);
+            this.buttonEdit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonEdit.Name = "buttonEdit";
-            this.buttonEdit.Size = new System.Drawing.Size(94, 23);
+            this.buttonEdit.Size = new System.Drawing.Size(70, 19);
             this.buttonEdit.TabIndex = 10;
             this.buttonEdit.Text = "Edit";
             this.buttonEdit.UseVisualStyleBackColor = true;
@@ -140,9 +151,10 @@
             // 
             // buttonDelete
             // 
-            this.buttonDelete.Location = new System.Drawing.Point(398, 68);
+            this.buttonDelete.Location = new System.Drawing.Point(298, 55);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(94, 23);
+            this.buttonDelete.Size = new System.Drawing.Size(70, 19);
             this.buttonDelete.TabIndex = 11;
             this.buttonDelete.Text = "Delete";
             this.buttonDelete.UseVisualStyleBackColor = true;
@@ -150,9 +162,10 @@
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(398, 96);
+            this.buttonSave.Location = new System.Drawing.Point(298, 78);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(94, 23);
+            this.buttonSave.Size = new System.Drawing.Size(70, 19);
             this.buttonSave.TabIndex = 12;
             this.buttonSave.Text = "Save";
             this.buttonSave.UseVisualStyleBackColor = true;
@@ -160,9 +173,11 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 332);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(378, 270);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.buttonEdit);
@@ -176,10 +191,14 @@
             this.Controls.Add(this.labelPhoneNumber);
             this.Controls.Add(this.labelLastName);
             this.Controls.Add(this.labelFirstName);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Contact Manager";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ContactManagerApp
 {
     public partial class Form1 : Form
     {
+        private const string FilePath = "contacts.txt";
+        private List<Contact> contacts = new List<Contact>();
+
         public Form1()
         {
             InitializeComponent();
+            LoadContacts();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string firstName = textBoxFirstName.Text.Trim();
+            string lastName = textBoxLastName.Text.Trim();
+            string phoneNumber = textBoxPhoneNumber.Text.Trim();
+            string email = textBoxEmail.Text.Trim();
+
+            if (!string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName))
+            {
+                Contact newContact = new Contact(firstName, lastName, phoneNumber, email);
+                contacts.Add(newContact);
+                listBoxContacts.Items.Add(newContact);
+                ClearInputFields();
+            }
+            else
+            {
+                MessageBox.Show("First Name and Last Name are required.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (listBoxContacts.SelectedIndex >= 0)
+            {
+                Contact selectedContact = (Contact)listBoxContacts.SelectedItem;
+                selectedContact.FirstName = textBoxFirstName.Text.Trim();
+                selectedContact.LastName = textBoxLastName.Text.Trim();
+                selectedContact.PhoneNumber = textBoxPhoneNumber.Text.Trim();
+                selectedContact.Email = textBoxEmail.Text.Trim();
+
+                listBoxContacts.Items[listBoxContacts.SelectedIndex] = selectedContact;
+                ClearInputFields();
+            }
+            else
+            {
+                MessageBox.Show("Please select a contact to edit.", "Edit Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxContacts.SelectedIndex >= 0)
+            {
+                contacts.RemoveAt(listBoxContacts.SelectedIndex);
+                listBoxContacts.Items.RemoveAt(listBoxContacts.SelectedIndex);
+                ClearInputFields();
+            }
+            else
+            {
+                MessageBox.Show("Please select a contact to delete.", "Delete Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveContacts();
+            MessageBox.Show("Contacts saved successfully.", "Save Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void LoadContacts()
+        {
+            if (File.Exists(FilePath))
+            {
+                string[] lines = File.ReadAllLines(FilePath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split('|');
+                    if (parts.Length == 4)
+                    {
+                        Contact contact = new Contact(parts[0], parts[1], parts[2], parts[3]);
+                        contacts.Add(contact);
+                        listBoxContacts.Items.Add(contact);
+                    }
+                }
+            }
+        }
+
+        private void SaveContacts()
+        {
+            List<string> lines = contacts.Select(c => $"{c.FirstName}|{c.LastName}|{c.PhoneNumber}|{c.Email}").ToList();
+            File.WriteAllLines(FilePath, lines);
+        }
+
+        private void ClearInputFields()
+        {
+            textBoxFirstName.Clear();
+            textBoxLastName.Clear();
+            textBoxPhoneNumber.Clear();
+            textBoxEmail.Clear();
+        }
+    }
+
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string firstName, string lastName, string phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} - {PhoneNumber} - {Email}";
         }
     }
 }
